9aa38ad75a30d98d6459efeb8a23e056223cf564
Bigizi <bigizinduwayo@gmail.com>
2023-01-06 12:36:13 +0200

    Fixing eslint and stylint errors

--- app.js
+++ app.js
@@ -11,48 +11,38 @@ document.querySelectorAll('.nav-links').forEach((n) => n.addEventListener('click
   navList.classList.remove('active');
 }));
 
-const form = document.getElementById("form");
+const form = document.getElementById('form');
 
-const email = document.getElementById("email");
+const email = document.getElementById('email');
 
-function setErrorFor(message) {
-  const formControl = document.querySelector('error');
-  const small = formControl.querySelector('small');
+// function setErrorFor(message) {
+//   const formControl = document.querySelector('error');
+//   const small = formControl.querySelector('small');
 
-  // add error message inside small
-  small.innerText = message;
-  // add error class
-  // formControl.classList = 'error';
-}
+//   // add error message inside small
+//   small.innerText = message;
+//   // add error class
+//   // formControl.classList = 'error';
+// }
 
 function isEmail(email) {
-  if(email.toLowerCase() === email) {
+  if (email.toLowerCase() === email) {
     return true;
-  } else {
-    return false;
   }
-  // let regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
-  // return regx.test(email);
+  return false;
 }
 
 function checkInputs(event) {
   // get the value from inputs
   const emailValue = email.value;
-
   // alert(emailValue);
-  if(isEmail(emailValue)) {
-    alert("Working");
+  if (isEmail(emailValue)) {
     return true;
-  } else {
-    // alert("not Working")
-    setErrorFor('Invalid Email, it should only contain lowercase');
-    event.preventDefault();
-    return false;
   }
+  // setErrorFor('Invalid Email, it should only contain lowercase');
+  document.querySelector('.small').innerHTML = 'Invalid email. It should only contain lowercace';
+  event.preventDefault();
+  return false;
 }
 
-form.addEventListener('submit', (e) => {
-   e.preventDefault();
-  
-  checkInputs();
-})
+form.addEventListener('submit', checkInputs);
