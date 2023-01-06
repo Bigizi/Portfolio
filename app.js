d7580d1f965fcf8d0046ea11f769d6134dfd527c
Bigizi <bigizinduwayo@gmail.com>
2023-01-06 13:02:20 +0200

    Removing unused code and commented code to make my code clean

--- app.js
+++ app.js
@@ -15,16 +15,6 @@ const form = document.getElementById('form');
 
 const email = document.getElementById('email');
 
-// function setErrorFor(message) {
-//   const formControl = document.querySelector('error');
-//   const small = formControl.querySelector('small');
-
-//   // add error message inside small
-//   small.innerText = message;
-//   // add error class
-//   // formControl.classList = 'error';
-// }
-
 function isEmail(email) {
   if (email.toLowerCase() === email) {
     return true;
@@ -35,10 +25,11 @@ function isEmail(email) {
 function checkInputs(event) {
   // get the value from inputs
   const emailValue = email.value;
-  // alert(emailValue);
+
   if (isEmail(emailValue)) {
     return true;
   }
+
   // setErrorFor('Invalid Email, it should only contain lowercase');
   document.querySelector('.small').innerHTML = 'Invalid email. It should only contain lowercace';
   event.preventDefault();
